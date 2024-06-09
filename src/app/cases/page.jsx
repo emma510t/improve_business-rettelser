ad574cb8c95250adcea3acbc5a9b20753fc98446
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2024-06-09 11:43:28 +0200

    case

--- src/app/cases/page.jsx
+++ src/app/cases/page.jsx
@@ -11,7 +11,7 @@ export default function cases() {
     <>
       <div className="bg-ibsilver-600 pb-8 md:pb-16 pt-[25px] md:pt-[40px] px-2.5 sm:px-4 md:px-6 lg:px-8 xl:px-10">
         <PageTagBreadcrumb currentPage="Cases" dark />
-        <H1 className="text-ibsilver-100">Cases</H1>
+        <H1 className="text-ibsilver-100">Udforsk Improve Business' tidligere arbejde</H1>
       </div>
       <section className="pb-8 md:pb-12 pt-[25px] md:pt-[40px] max-w-[1280px] w-full px-2.5 sm:px-4 md:px-6 lg:px-8 xl:px-10 mx-auto">
         <H2 className="text-ibsilver-600">Se vores cases</H2>
