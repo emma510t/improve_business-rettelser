327dcfc826e07be7624628d5dc70ebbb2a7c8e69
Emil Kristensen <emil00y1@stud.kea.dk>
2024-06-09 15:46:38 +0200

    added maxwidth

--- src/app/cases/page.jsx
+++ src/app/cases/page.jsx
@@ -17,7 +17,7 @@ export default function cases() {
       </div>
       <section className="pb-8 md:pb-12 pt-[25px] md:pt-[40px] max-w-[1280px] w-full px-2.5 sm:px-4 md:px-6 lg:px-8 xl:px-10 mx-auto">
         <H2 className="text-ibsilver-600">Se vores cases</H2>
-        <P>
+        <P className="max-w-[640px]">
           Se vores tidligere arbejde ved at klikke ind på de enkelte cases
           nedenfor og opdag, hvordan vi har hjulpet virksomheder med at nå deres
           mål og skabe vækst.
